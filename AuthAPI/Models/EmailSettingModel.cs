f79b23adbfbcdd3f69c547fe46f186af0d5a4f99
M-Zaheer-Khatri <muhammad92zaheer786@gmail.com>
2025-04-05 00:54:23 +0400

    Commit_00005 - Fixed spelling correct.

--- AuthAPI/Models/EmailSettingModel.cs
+++ AuthAPI/Models/EmailSettingModel.cs
@@ -6,6 +6,6 @@
         public string From { get; set; } = default!;
         public string SmtpServer { get; set; } = default!;
         public int Port { get; set; }
-        public bool EnablSSl { get; set; }
+        public bool EnablSsl { get; set; }
     }
 }
